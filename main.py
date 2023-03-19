125a2a47e613b1fc19d9fa24bcc3d35d978a7bf3
Dominik Jambrović <dominik.jambrovic@gmail.com>
2023-03-19 20:02:38 +0100

    stats

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,48 +6,12 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
+from attack_funcs import attack_pgd
 from ResidualNetwork18 import ResidualNetwork18
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-if __name__ == "__main__":
-
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    ])
-
-    train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
-    test_data = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
-
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=512, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=False)
-
-    classes_map = {
-        0: "airplane",
-        1: "automobile",
-        2: "bird",
-        3: "cat",
-        4: "deer",
-        5: "dog",
-        6: "frog",
-        7: "horse",
-        8: "ship",
-        9: "truck",
-    }
-
-    model = ResidualNetwork18().to(device)
-
-    loss_calc = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.1)
-
+def train():
     for epoch in range(10):
         print(f"Starting epoch: {epoch + 1}")
 
@@ -75,27 +40,92 @@ if __name__ == "__main__":
         print(f"Total train loss for epoch {epoch+1}: {total_train_loss}")
         print(f"Total train accuracy for epoch {epoch+1}: {100 * train_correct/train_total}")
 
-        #Test
-        model.eval()
+        test(epoch)
 
-        total_test_loss = 0
-        test_correct = 0
-        test_total = 0
 
-        with torch.no_grad():
-            for (x, y) in test_loader:
-                x = x.to(device)
-                y = y.to(device)
+def test(curr_epoch):
+    model.eval()
 
-                y_ = model(x)
-                loss = loss_calc(y_, y)
+    total_test_loss = 0
+    test_correct = 0
+    test_total = 0
 
-                total_test_loss += loss
-                _, y_ = y_.max(1)
-                test_total += y.size(0)
-                test_correct += y_.eq(y).sum().item()
+    with torch.no_grad():
+        for (x, y) in test_loader:
+            x = x.to(device)
+            y = y.to(device)
 
-        print(f"Total test loss for epoch {epoch+1}: {total_test_loss}")
-        print(f"Total test accuracy for epoch {epoch+1}: {100 * test_correct/test_total}")
+            y_ = model(x)
+            loss = loss_calc(y_, y)
 
-    torch.save(model, './models/resnet18.txt')
+            total_test_loss += loss
+            _, y_ = y_.max(1)
+            test_total += y.size(0)
+            test_correct += y_.eq(y).sum().item()
+
+    print(f"Total test loss for epoch {curr_epoch+1}: {total_test_loss}")
+    print(f"Total test accuracy for epoch {curr_epoch+1}: {100 * test_correct/test_total}")
+
+def test_robustness():
+    model.eval()
+
+    adv_total = 0
+    adv_correct = 0
+
+    with torch.no_grad():
+        for (x, y) in test_loader:
+            x = x.to(device)
+            y = y.to(device)
+            adversarial = attack_pgd(model, x, y, eps=0.3, koef_it=0.05, steps=5)
+
+            y_ = model(adversarial)
+            _, y_ = y_.max(1)
+            adv_total += y.size(0)
+            adv_correct += y_.eq(y).sum().item()
+
+    print(f"Accuracy on adversarial examples generated using PGD attack: {100 * adv_correct/adv_total}")
+
+
+if __name__ == "__main__":
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
+    test_data = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=256, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=False)
+
+    classes_map = {
+        0: "airplane",
+        1: "automobile",
+        2: "bird",
+        3: "cat",
+        4: "deer",
+        5: "dog",
+        6: "frog",
+        7: "horse",
+        8: "ship",
+        9: "truck",
+    }
+
+    model = ResidualNetwork18().to(device)
+
+    loss_calc = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+    #train()
+    #torch.save(model, './models/resnet18.txt')
+
+    torch.load(model, './models/resnet18.txt')
+    test_robustness()
